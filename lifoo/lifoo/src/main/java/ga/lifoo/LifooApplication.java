c5d66f3fd59993ce26b66bc9d463937e0e479917
jsungmin6 <jsungmin506@gmail.com>
2021-02-20 17:30:29 +0900

    lifoo project init

---
+++ lifoo/lifoo/src/main/java/ga/lifoo/LifooApplication.java
@@ -0,0 +1,13 @@
+package ga.lifoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifooApplication.class, args);
+	}
+
+}
---
+++ lifoo/lifoo/src/test/java/ga/lifoo/LifooApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.lifoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

12064b0c5242f522a773b68efc9aa096704214d5
jsungmin6 <jsungmin506@gmail.com>
2021-02-24 20:35:41 +0900

    add 자동 로그인 API

---
+++ lifoo/lifoo/src/main/java/ga/lifoo/src/user/models/GetJwtReq.java
@@ -0,0 +1,12 @@
+package ga.homealoneapi.src.user.models;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PUBLIC) // Unit Test 를 위해 PUBLIC
+@Getter
+public class GetJwtReq {
+
+    String jwt;
+}
